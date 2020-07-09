90f17189ae391909fa2e835afa9c6c97db2c1f11
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2020-07-09 16:48:12 +0530

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -1,1 +1,19 @@
-uyyy
+#!/usr/bin/groovy
+def call() {
+  def SCANNER_HOME = tool 'sonar scanner'
+  withSonarQubeEnv('sonar') {
+    sh "${SCANNER_HOME}/bin/sonar-scanner \
+      -Dsonar.host.url=https://sonarcloud.io/  \
+      -Dsonar.login = 2ccda422b3b6c622bf06b58f5ffeb6402b79bdbd \
+      -Dsonar.organization=samlee \
+      -Dsonar.projectKey=samlee_sonar_analysis \
+      -Dsonar.projectName=samlee_sonar_analysis \
+      -Dsonar.projectVersion=1.0 \
+      -Dsonar.sources=. \
+      -Dsonar.language=java \
+      -Dsonar.java.binaries=. \
+      -Dsonar.java.coveragePlugin=jacoco \
+      -Dsonar.coverage.jacoco.xmlReportPaths = target/site/jacoco/*.xml \
+      -Dsonar.exclusions = **/*.html,**/*.css,**/*.js,**/*.xml,src/test/java/com/javatechie/StringPalindrome/AppTest.java \
+      -Dsonar.sourceEncoding=UTF-8 \
+     
