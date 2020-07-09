bac112cac738a769481c7911b75691fe1a3997be
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2020-07-09 15:52:07 +0530

    updated scm

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,1 +1,8 @@
-abc
+def call(Map stageParams) {
+
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name:  stageParams.branch_name ]],
+        userRemoteConfigs: [[ url: stageParams.url_name ]]
+    ])
+}
