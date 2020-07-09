6fb8f976d5fdd92a1cb9c88ae04a5536c11fbbf6
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2020-07-09 20:08:02 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,8 +1,18 @@
-def call(Map stageParams) {
+def call(branch, url) {
 
+    
+  def branchName = branch
+   def urllink = url
+    
+    
+    echo "$branchName"
+    echo "$urllink"
     checkout([
         $class: 'GitSCM',
-        branches: [[name:  stageParams.branch_name ]],
-        userRemoteConfigs: [[ url: stageParams.url_name ]]
+      //  branches: [[name:  stageParams.branch_name ]],
+      //  userRemoteConfigs: [[ url: stageParams.url_name ]]
+        
+        branches: [[name: branchName ]],
+        userRemoteConfigs: [[ url: urllink ]]
     ])
 }
