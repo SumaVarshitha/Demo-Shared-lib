d9f452afbce4058c95338cd42453867b9e89137a
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2020-07-15 15:54:37 +0530

    Update sonar_analyse.groovy

--- vars/sonar_analyse.groovy
+++ vars/sonar_analyse.groovy
@@ -1,33 +1,21 @@
-#!/usr/bin/env groovy
-
-
-def call() {
+#!/usr/bin/groovy
+def call(sonarorganization,sonarProjectkey,sonarProjectname) 
   def SCANNER_HOME = tool 'sonar scanner'
   withSonarQubeEnv('sonar') {
-   sh """
-${SCANNER_HOME}/bin/sonar-scanner \
--Dsonar.host.url=https://sonarcloud.io/ \
--Dsonar.login=2ccda422b3b6c622bf06b58f5ffeb6402b79bdbd \
--Dsonar.organization=samlee \
--Dsonar.projectKey=samlee_sonar_analysis \
--Dsonar.projectName=samlee_sonar_analysis \
- -Dsonar.projectVersion=1.0 \
--Dsonar.language=java \
--Dsonar.sources=. \
--Dsonar.junit.reportsPath=target/surefire-reports \
--Dsonar.surefire.reportsPath=target/surefire-reports \
--Dsonar.jacoco.reportPath=target/jacoco.exec \
--Dsonar.exclusions=**/*.html,**/*.css,**/*.js,**/*.xml,src/test/java/com/javatechie/StringPalindrome \
--Dsonar.binaries=target/classes \
--Dsonar.java.coveragePlugin=jacoco \
--Dsonar.sourceEncoding=UTF-8 \
-
-
-""" 
-}
-
- 
-
- 
+    sh "${SCANNER_HOME}/bin/sonar-scanner
+                -Dsonar.host.url=https://sonarcloud.io/ \
+                -Dsonar.login = 2ccda422b3b6c622bf06b58f5ffeb6402b79bdbd \
+                -Dsonar.organization=${sonarorganization} \
+                -Dsonar.projectKey=${sonarProjectkey} \
+                -Dsonar.projectName=${sonarProjectname} \
+                -Dsonar.projectVersion=1.0 \
+                -Dsonar.sources=. \
+                -Dsonar.language=java \
+                -Dsonar.java.binaries=. \
+                -Dsonar.java.coveragePlugin=jacoco \
+                -Dsonar.coverage.jacoco.xmlReportPaths=target/site/jacoco/*.xml \
+                -Dsonar.exclusions=**/*.html,**/*.css,**/*.js,src/test/java/com/javatechie/StringPalindrome/AppTest.java \
+                -Dsonar.sourceEncoding=UTF-8"
+    }  
 }
      
