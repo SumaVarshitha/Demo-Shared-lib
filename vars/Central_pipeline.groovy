d67c1b1dbcf3f802ca2b24fef6dae4ee7f9aa345
Samlee Mohanty <50711741+Samlee97@users.noreply.github.com>
2020-07-15 16:21:01 +0530

    Create Central_pipeline.groovy

---
+++ vars/Central_pipeline.groovy
@@ -0,0 +1,38 @@
+#!/usr/bin/env groovy
+def call(branchName,urllink,dockerimage,mavenBuild,sonarorganization,sonarprojectKey,sonarprojectName){
+        pipeline {
+            agent any
+            tools {
+                   maven 'Maven_Home'
+            }
+            stages {
+               
+                stage('CleanUp'){
+                    steps{
+                         deleteDir()
+                    }
+                }
+                stage('Code Checkout'){
+                    steps{
+                           gitCheckout((branchName,urllink)
+                    }
+                }
+               
+                stage('Build_In_Container'){
+                    steps{
+                            maven_build(dockerimage,mavenBuild)
+                    }
+                }
+                stage('SonarStage'){
+                    steps{
+                       sonar_analyse(sonarorganization,sonarprojectKey,sonarprojectName)
+                    }
+                }
+                stage('QualityGate'){
+                    steps{
+                       sonar_quality_gate()
+                    }
+                }
+            }
+      }
+}
